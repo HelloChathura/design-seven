0809b569f05daf8dab2230485687fabc87a938f8
HelloChathura <chathucj@gmail.com>
2024-09-25 22:37:11 +0530

    vercel speed insight added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ContactUs from "./components/contact-us";
 import About from "./components/about";
 import FlippingCard from "./components/flipping-cards"
 import AdvantagesCard from "./components/advantages"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Design Seven - Home",
@@ -28,6 +29,7 @@ export default function Home() {
 <FlippingCard/>
 <AdvantagesCard />
 <ContactUs/>
+<SpeedInsights/>
    </>
   );
 }
