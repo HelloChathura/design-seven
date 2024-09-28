abbf5ca66f9eab6c418c938c67de1bc859c1e66a
HelloChathura <chathucj@gmail.com>
2024-09-29 01:44:37 +0530

    Mail Configuration Added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,12 @@
+import { Lightbulb, Target, Heart } from "lucide-react";
+import { Card } from "../components/ui/card";
 import type { Metadata } from 'next'
+import dynamic from 'next/dynamic';
 
+const ContactUs = dynamic(() => import('../components/contact-us'), { ssr: false });
+
+
+//SEO
 export const metadata: Metadata = {
     title: "Design Seven - Contact Us",
     keywords: [
@@ -13,11 +20,39 @@ export const metadata: Metadata = {
     authors: [{ name: "Design Seven" }],
     description:
       "Design Seven has established itself as a BIM consultancy and Design Documentation Services for the Architectural, Interior Design industries.",
-  };  
-export default function contact() {
-    return (
-     <>
- <p>Coming Soon....</p>
-     </>
-    );
-  }
+  }; 
+
+export default function About() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div
+        className="relative h-[600px] bg-cover bg-center"
+        style={{ backgroundImage: "url('/contact-image.jpg')" }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="text-center text-white px-6">
+            <h1 className="text-3xl md:text-4xl mb-4">Contact Us</h1>
+            <p className="text-xl md:text-xl max-w-3xl mx-auto leading-relaxed">
+              Contact Us
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 py-8">
+
+
+        <div className="max-w-9xl mx-auto mt-2">
+        <ContactUs showBackgroundImage={false} />
+          {/* <Card className="shadow-lg rounded-xl transition-all duration-500 hover:shadow-2xl">
+            <div className="p-8">
+              <h2 className="text-2xl mb-8 text-center">Contact Us</h2>
+              <ContactUs showBackgroundImage={false} />
+            </div>
+          </Card> */}
+        </div>
+
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 <About/>
 <FlippingCard/>
 <AdvantagesCard />
-<ContactUs/>
+<ContactUs showBackgroundImage={true}/>
    </>
   );
 }
